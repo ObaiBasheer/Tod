10a64968cbfbe5b32783b12b23c134aa8445252f
oahmed <oahmed@ncec.gov.sa>
2024-12-13 22:14:33 +0300

    introduce tutorial on movement

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-
-
-
 class V2{
     constructor(x,y){
         this.x = x;
@@ -18,9 +15,115 @@ class V2{
     scale(s) {
         return new V2(this.x * s, this.y * s);
     }
+
+    length() {
+        return this.x * this.x + this.y * this.y;
+    }
 }
 
 
+const speed = 1000;
+const radius = 50;
+
+const directionMap = {
+    's': new V2(0,1.0),
+    'w': new V2(0,-1.0),
+    'a': new V2(-1.0,0),
+    'd': new V2(1.0,0),
+}
+
+
+class TutorialPopup{
+    constructor(text){
+        this.dalpha = 0.0;
+        this.alpha = 0.0;
+        this.text = text;
+    }
+
+    update(dt) {
+    this.alpha += this.dalpha * dt;
+
+        if (this.dalpha < 0.0 && this.alpha <= 0.0) {
+            this.delta = 0.0;
+            this.alpha = 0.0;
+        } else if (this.alpha > 0.0 && this.alpha >= 1.0) {
+            this.delta = 0.0;
+            this.alpha = 1.0;
+        }
+    }
+    render(ctx) {
+        const width = ctx.canvas.width;
+        const height = ctx.canvas.height;
+
+        ctx.fillStyle = `rgba(255, 255, 255, ${this.alpha})`;
+        ctx.font = '30px LexendMega';
+        ctx.textAlign = 'center';
+        ctx.fillText(this.text, width / 2,height / 2);
+    }
+
+    fadeIn() {
+        this.dalpha = 1.0;
+    }
+    fadeOut() {
+        this.dalpha = -1.0;
+    }
+}
+
+
+class Game {
+    constructor(){
+        this.pos = new V2(radius + 10, radius + 10);
+        this.keyPressed = new Set();
+        this.popup = new TutorialPopup("WASD to move around");
+        this.popup.fadeIn()
+        this.player_learned_how_to_move = false;
+    }
+
+    update(dt) {
+        let vel = new V2(0,0);
+
+        for(let key of this.keyPressed){
+            console.log(key);
+            if(key in directionMap) {
+                console.log(true);
+
+                vel =vel.add( directionMap[key].scale(speed));
+            }
+        }
+
+        if(!this.player_learned_how_to_move && vel.length() > 0.0){
+            this.player_learned_how_to_move = true;
+            this.popup.fadeOut();
+        }
+        this.pos = this.pos.add(vel.scale(dt));
+        this.popup.update(dt);
+
+
+
+    }
+    render(ctx) {
+        const width = ctx.canvas.width;
+        const height = ctx.canvas.height;
+
+        ctx.clearRect(0, 0, width, height);
+
+
+        fillCircle(ctx, this.pos, radius, 'red');
+
+        this.popup.render(ctx);
+
+
+
+    }
+    keyDown(e) {
+        this.keyPressed.add(e.key);
+
+    }
+    keyUp(e) {
+        this.keyPressed.delete(e.key);
+
+    }
+}
 
 
 function fillCircle(ctx,center, radius, color= 'green') {
@@ -30,90 +133,50 @@ function fillCircle(ctx,center, radius, color= 'green') {
     ctx.fillStyle = color;
     ctx.fill();
 }
+
+
+
 (() => {
     'use strict';
     console.log('Happy developing ✨')
 
     const canvas = document.querySelector('#game');
-    const radius = 50;
     const context = canvas.getContext('2d');
-    const speed = 1000;
-
+    const game = new Game();
 
     let start;
-    let pos = new V2(radius + 10, radius + 10);
-
-    let directionMap = {
-        's': new V2(0,speed),
-        'w': new V2(0,-speed),
-        'a': new V2(-speed,0),
-        'd': new V2(speed,0),
-    }
-
-    let keyPressed = new Set();
 
 
     function step(timestamp) {
         if(start === undefined) {
             start = timestamp;
         }
-        const elapsedTime = (timestamp - start) * 0.001;
+
+        const dt = (timestamp - start) * 0.001;
         start = timestamp;
+
         const width = window.innerWidth;
         const height = window.innerHeight;
         canvas.width = width;
         canvas.height = height;
 
-
-        let vel = new V2(0,0);
-
-        for(let key of keyPressed){
-            console.log(key);
-            if(key in directionMap) {
-                console.log(true);
-
-                vel =vel.add( directionMap[key]);
-            }
-        }
-
-
-
-
-
-        pos = pos.add(vel.scale(elapsedTime));
-
-
-
-
-        context.clearRect(0, 0, width, height);
-        fillCircle(context, pos, radius, 'red');
+        game.update(dt);
+        game.render(context);
 
         window.requestAnimationFrame(step);
 
-
     }
+    window.requestAnimationFrame(step);
 
     document.addEventListener('keydown', (event) => {
-
-        keyPressed.add(event.key);
-
+        game.keyDown(event);
     });
 
 
 
     document.addEventListener('keyup', (event) => {
-        keyPressed.delete(event.key);
-
-        }
-    );
-
-    window.requestAnimationFrame(step);
-
-
-
-
-
-
+        game.keyUp(event);
+    });
 
 
 })()
